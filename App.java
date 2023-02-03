6c6601d4f56f057851f4051f694cd7e41e04f5fd
LuisAMoralesA <124367893+LuisAMoralesA@users.noreply.github.com>
2023-02-03 08:15:05 -0600

    Update App.java

--- App.java
+++ App.java
@@ -5,6 +5,8 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HELLO WORLD !!"); //sysout, cntl + Barra espaciadora
+		//1. Paquetes siempre seran com.pp2.
+		//2. La clase siempre se llamara App.
 	}
 
 }
